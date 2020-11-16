02898df803bac2c3aceb1d9cbccaecf6e7e4cd4c
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2020-11-16 18:51:07 +0000

    database

--- db/index.js
+++ db/index.js
@@ -1,3 +1,10 @@
 const { Pool } = require("pg");
-const pool = new Pool( { ssl:{rejectUnauthorized: false} });
-module.exports = { query:(text, params, callback) => {return pool.query(text, params, callback) } };
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
