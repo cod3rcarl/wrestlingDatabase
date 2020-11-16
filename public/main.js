546e84a60d31be81bfbefaa9c2c595344e91cdd6
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2020-11-16 18:48:11 +0000

    working database

--- public/main.js
+++ public/main.js
@@ -1,46 +1,38 @@
-
 const submitButton = document.querySelector("#submit-button");
 let dateValue = document.querySelector("#dateInput");
-const superstar = document.querySelector(".superstar")
+const superstar = document.querySelector(".superstar");
 
 // const allDataButton = document.querySelector("#displayAllData")
 
-
-submitButton.addEventListener('click', wrestlerInfo);
+submitButton.addEventListener("click", wrestlerInfo);
+console.log("working");
 //allDataButton.addEventListener('click', bootCamperInfo);
 
-
-async function wrestlerInfo() { 
+async function wrestlerInfo() {
   let value = dateValue.value;
 
-  const response = await fetch('http://localhost:5000/wrestlers');
+  const response = await fetch("http://localhost:5000/wrestlers");
   const data = await response.json();
-  console.log(data)
+
   data.payload.forEach((item) => {
-console.log(item)
-  let dateOne = item.datelost;
- 
-  let dateTwo = item.datewon;
-  
+    let dateOne = item.datelost;
+    let dateTwo = item.datewon;
 
-  const champion = item.champion;
+    const champion = item.champion;
 
-  D_1 = dateOne.split("/"); 
-  D_2 = dateTwo.split("/"); 
-  D_3 = value.split("/"); 
-  
-    
-  let dlost = new Date(D_1[2], parseInt(D_1[1]) - 1, D_1[0]);
- 
-  let dwon = new Date(D_2[2], parseInt(D_2[1]) - 1, D_2[0]); 
-  
-  let d3 = new Date(D_3[2], parseInt(D_3[1]) - 1, D_3[0]); 
- 
-    
-  if (d3 < dlost && d3 > dwon) { 
-    console.log("success")
-    superstar.innerText = champion
-  } 
-})
+    D_1 = dateOne.split("/");
+    D_2 = dateTwo.split("/");
+    D_3 = value.split("/");
 
-};
+    let dlost = new Date(D_1[2], parseInt(D_1[1]) - 1, D_1[0]);
+
+    let dwon = new Date(D_2[2], parseInt(D_2[1]) - 1, D_2[0]);
+
+    let d3 = new Date(D_3[2], parseInt(D_3[1]) - 1, D_3[0]);
+
+    if (d3 < dlost && d3 > dwon) {
+      console.log("success");
+      superstar.innerText = champion;
+    }
+  });
+}
